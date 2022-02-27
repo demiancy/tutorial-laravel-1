4e7b87cfd18b3beeae8a96cf449d32fe4b7a4766
cyia <demiancy.dev@gmail.com>
2022-02-27 20:37:24 -0300

    Migration and model for categories

--- app/database/migrations/2022_02_25_203810_create_categories_table.php
+++ app/database/migrations/2022_02_25_203810_create_categories_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->string('image', 55)->nullable();
+            $table->string('image', 100)->nullable();
             $table->timestamps();
         });
     }
