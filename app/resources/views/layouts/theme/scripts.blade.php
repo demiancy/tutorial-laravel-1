2aeba9bae370f22c9782f2e11ea31e4d807608b7
cyia <demiancy.dev@gmail.com>
2022-03-02 14:44:50 -0300

    Added javascript function for notifications and unused scripts removed

--- app/resources/views/layouts/theme/scripts.blade.php
+++ app/resources/views/layouts/theme/scripts.blade.php
@@ -1,6 +1,4 @@
 <script src="{{ asset('theme/js/loader.js') }}" defer></script>
-
-<!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" defer></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous" defer></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" defer></script>
@@ -14,11 +12,22 @@
 </script>
 
 <script src="{{ asset('theme/js/custom.js') }}" defer></script>
-<!-- END GLOBAL MANDATORY SCRIPTS -->
 
-<!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
-<script src="{{ asset('theme/plugins/apex/apexcharts.min.js') }}" defer></script>
-<script src="{{ asset('theme/js/dashboard/dash_2.js') }}" defer></script>
-<!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
+<script src="{{ asset('theme/plugins/sweetalerts/sweetalert2.min.js') }}" defer></script>
+<script src="{{ asset('theme/plugins/notification/snackbar/snackbar.min.js') }}" defer></script>
+<script src="{{ asset('theme/plugins/nicescroll/nicescroll.min.js') }}" defer></script>
+<script src="{{ asset('theme/plugins/currency/currency.min.js') }}" defer></script>
 
 <script src="{{ asset('js/app.js') }}" defer></script>
+
+<script>
+    function noty(msg, option = 1) {
+        Snackbar.show({
+            text: msg.toUpperCase(),
+            actionText: 'Cerrar',
+            actionTextColor: '#fff',
+            backgroundColor: option == 1 ? '#3b3f5c' : '#e7515a',
+            pos: 'top-right'
+        });
+    }
+</script>
