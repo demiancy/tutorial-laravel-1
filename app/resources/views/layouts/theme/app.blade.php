5ad2fa99d7a8138832d801730af0b6ee6e35c7c2
cyia <demiancy.dev@gmail.com>
2022-03-04 21:42:41 -0300

    Added Font Awesome icons

--- app/resources/views/layouts/theme/app.blade.php
+++ app/resources/views/layouts/theme/app.blade.php
@@ -18,6 +18,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&display=swap" rel="stylesheet">
+    <link href="{{ asset('vendor/fontawesome/css/all.css') }}" rel="stylesheet">
 </head>
 
 <body class="dashboard-analytics">
