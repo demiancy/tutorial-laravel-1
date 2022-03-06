a700b59c0062156705d7d4820c8b5f810220ab03
cyia <demiancy.dev@gmail.com>
2022-03-06 20:29:49 -0300

    Added modal head and footer

--- app/resources/views/common/modalHead.blade.php
+++ app/resources/views/common/modalHead.blade.php
@@ -5,7 +5,7 @@
                 <h5 class="modal-title text-white">
                     <span class="font-weight-bold">{{ $componentName }}</span> | {{ $selected_id > 0 ? 'EDITAR' : 'CREAR'  }}
                 </h5>
-                </h6 class="text-center text-warning">
+                </h6 class="text-center text-warning" wire:loading>
                     POR FAVOR ESPERE
                 </h6>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
