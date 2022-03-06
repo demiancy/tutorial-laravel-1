43a51b90cc88cbdcc880ce0f0604b71b7f186fd6
cyia <demiancy.dev@gmail.com>
2022-03-06 20:37:33 -0300

    Added search component

---
+++ app/resources/views/common/searchBox.blade.php
@@ -0,0 +1,12 @@
+<div class="row justify-content-between">
+    <div class="col-md-4 col-12">
+        <div class="input-group mb-4">
+            <div class="input-group-prepend">
+                <span class="input-group-text input-gp">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </span>
+            </div>
+            <input type="text" wire:model="search" plaseholder="Buscar" class="form-control">
+        </div>
+    </div>
+</div>
